045ceccbbd0cc2f39a93e1a914deb0bf906e84d2
csbenk <csbhaskar95@gmail.com>
2018-08-08 14:51:19 +0530

    second commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,11 +13,12 @@ TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
